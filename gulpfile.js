2a1895c15918b566c26f985c22b781c2499f2612
SergKazakov <sergeykazakoff@yandex.ru>
2015-02-22 22:08:33 +0300

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    livereload    = require('gulp-livereload'),
+    sourcemaps    = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  gulp.src('public/scss/main.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: true
+  }))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('public/css'))
+  .pipe(livereload());
+});
+
+gulp.task('default', ['sass'], function(){
+  livereload.listen();
+  gulp.watch('public/**/*.scss', ['sass']);
+});
